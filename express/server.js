4f1137907e2d646098fe352b5a495f45355bbcb5
codecheap2019 <52380928+codecheap2019@users.noreply.github.com>
2019-12-08 20:32:01 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -12,7 +12,9 @@ router.get('/', (req, res) => {
   res.end();
 });
 router.get('/about',(req, res) => {
-  res.send("This is About"+req.id);
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from About.js!</h1>');
+  res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
